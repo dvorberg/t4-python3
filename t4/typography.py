41bfbfd4a17110e1519491f73639b0c2c28375a0
Diedrich Vorberg <diedrich@tux4web.de>
2022-12-04 18:50:26 +0100

    Added parse_ and pretty_german_float()

--- t4/typography.py
+++ t4/typography.py
@@ -1,4 +1,4 @@
-import datetime, re, html
+import datetime, re, html, decimal
 
 from .sql import normalize_whitespace
 
@@ -10,7 +10,7 @@ date_until_re = re.compile(r'(\d+)\.-(\d+)\.')
 
 def improve_typography(content, lang="de"):
     if lang == "de":
-        # "gerade" und ,,typografische'' Anführungszeichen    
+        # "gerade" und ,,typografische'' Anführungszeichen
         content = re.sub(opening_quote_re, u"\\1„\\2", content)
         content = re.sub(closing_quote_re, u"\\1“\\2", content)
         content = re.sub(opening_single_quote_re, u"\\1‚\\2", content)
@@ -37,7 +37,7 @@ def improve_typography(content, lang="de"):
     # Ellipsis
     content = content.replace(" ...", " …")
     content = content.replace("...", "…")
-    
+
     return content
 
 def add_web_paragraphs(s, use_ps=True):
@@ -49,15 +49,16 @@ def add_web_paragraphs(s, use_ps=True):
     be quoted.
     """
     s = html.escape(s)
-    
+
     s = s.replace("\r\n", "\n")
+    s = s.replace("\r", "\n")
     ps = s.split("\n\n")
 
     ps = [p.replace("\n", "<br />\n") for p in ps]
 
     if use_ps:
         ps = ["<p>" + p + "</p>" for p in ps]
-    
+
     return "\n\n".join(ps)
 
 def pretty_bytes(bytes):
@@ -134,4 +135,61 @@ def parse_german_date(s):
         day, month, year = [ int(n) for n in match.groups() ]
         return datetime.date(year, month, day)
 
-    
+
+def parse_german_float(s):
+    """
+    Parse a string containing a German float-point number (.s separate
+    1000s, and decimal comma) into a float.
+    """
+    if type(s) is str:
+        # Replace , with .
+        s = s.replace(",", ".")
+
+        # Remove all .s except the last one.
+        parts = s.split(".")
+        if len(parts) > 2:
+            s = ".".join(parts[:-1]) + "." + parts[-1]
+
+    return float(s)
+
+def pretty_german_float(f, decimals=2, form=False):
+    """
+    Return a German representation of a float point number as a
+    string.
+    """
+    if type(f) != float and not isinstance(f, decimal.Decimal):
+        if form:
+            if f is None:
+                return ""
+            else:
+                try:
+                    f = parse_german_float(f)
+                except ValueError:
+                    return f
+        else:
+            raise TypeError("Don’t know how to convert " + str(type(f)))
+
+    # f = float(f) Don’t do that. If this is a decimal.Decimal, we’d loose
+    # precision!
+
+    if f < 0:
+        negative = True
+        f *= -1
+    else:
+        negative = False
+
+    s = "%f" % f
+    euros, cents = s.split(".")
+
+    while cents.endswith("0"):
+        cents = cents[:-1]
+
+    if cents:
+        s = euros + "," + cents
+    else:
+        s = euros
+
+    if negative:
+        return "-" + s
+    else:
+        return s
